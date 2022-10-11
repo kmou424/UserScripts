d868d02b905719bf4c6a475f80db605ed987c997
kmou424 <me@kmou424.moe>
2022-10-11 21:42:18 +0800

    fix: GlobalBlankHyperlinkPatcher: Fix up erro when target is empty

--- GlobalBlankHyperlinkPatcher.js
+++ GlobalBlankHyperlinkPatcher.js
@@ -20,11 +20,16 @@
     if (!ALLOWED_DOMAINS.includes(domain)) return;
 
     setInterval(() => {
-            let all_a_tags = document.getElementsByTagName('a');
-            for (let i = 0; i < all_a_tags.length; ++i) {
-                if (all_a_tags[i] != undefined && !all_a_tags[i].target.includes("_blank")) {
-                    all_a_tags[i].target = `${all_a_tags[i].target} _blank`;
+        let all_a_tags = document.getElementsByTagName('a');
+        for (let i = 0; i < all_a_tags.length; ++i) {
+            console.log(all_a_tags[i].target.length);
+            if (all_a_tags[i] != undefined && !all_a_tags[i].target.includes("_blank")) {
+                if (all_a_tags[i].target.length != 0) {
+                    all_a_tags[i].target += " _blank";
+                } else {
+                    all_a_tags[i].target = "_blank";
                 }
             }
+        }
     }, 1000);
 })();
