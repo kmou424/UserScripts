de359393cba6542189ba5f93992e35a2d16061de
kmou424 <me@kmou424.moe>
2022-10-11 22:29:05 +0800

    chore: GlobalBlankHyperlinkPatcher: Add [P] signal to hyperlink for sign as patched

--- GlobalBlankHyperlinkPatcher.js
+++ GlobalBlankHyperlinkPatcher.js
@@ -22,12 +22,18 @@
     setInterval(() => {
         let all_a_tags = document.getElementsByTagName('a');
         for (let i = 0; i < all_a_tags.length; ++i) {
-            console.log(all_a_tags[i].target.length);
             if (all_a_tags[i] != undefined && !all_a_tags[i].target.includes("_blank")) {
                 if (all_a_tags[i].target.length != 0) {
                     all_a_tags[i].target += " _blank";
                 } else {
                     all_a_tags[i].target = "_blank";
+                }
+                if (all_a_tags[i].innerText.length > 0
+                    && all_a_tags[i].href.length > 0
+                    && all_a_tags[i].href != document.URL
+                    && !all_a_tags[i].href.startsWith(document.URL + "#")
+                    && !all_a_tags[i].href.startsWith("javascript:void(0)")) {
+                    all_a_tags[i].innerText = "[P]" + all_a_tags[i].innerText;
                 }
             }
         }
