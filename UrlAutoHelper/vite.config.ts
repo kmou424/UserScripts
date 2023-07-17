4f8dbcb4655006ac7cf721cd4a01dc5ca4662399
kmou424 <me@kmou424.moe>
2023-07-17 16:51:29 +0800

    chore(config): UrlAutoHelper: update info

--- UrlAutoHelper/vite.config.ts
+++ UrlAutoHelper/vite.config.ts
@@ -3,16 +3,19 @@ import monkey from 'vite-plugin-monkey';
 // @ts-ignore resolveJsonModule
 import {name, version} from './package.json';
 // @ts-ignore resolveJsonModule
-import {repoUrl} from '../config.json'
+import {repoUrl} from '../config.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [monkey({
     entry: 'src/main.ts', userscript: {
       "author": 'kmou424',
+      "downloadURL": `${repoUrl}/${name}.user.js`,
       "icon": 'https://vitejs.dev/logo.svg',
       "match": ['*://*/*'],
-      "run-at": "document-end",
+      "name": 'UrlAutoHelper',
+      "namespace": 'https://github.com/kmou424/TampermonkeyScripts',
+      "run-at": 'document-end',
       "updateURL": `${repoUrl}/${name}.user.js`,
       "version": `${version}-${process.env.VERSION_CODE}`
     },
